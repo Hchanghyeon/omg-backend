c49f9dad524c527751159ddcf16ed7ecf38fdbd8
changhyeon <changhyeon.h@kakao.com>
2024-02-03 20:51:27 +0900

    feat: RestControllerAdvice 구현

---
+++ src/main/java/com/chang/omg/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.chang.omg.global.exception;
+
+import static com.chang.omg.global.exception.GlobalExceptionCode.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(final Exception exception) {
+        log.error("{}", GLOBAL_INTERNAL_SERVER_ERROR, exception);
+
+        return ResponseEntity.status(GLOBAL_INTERNAL_SERVER_ERROR.getStatus())
+                .body(new ErrorResponse(GLOBAL_INTERNAL_SERVER_ERROR.getCode()));
+    }
+}
