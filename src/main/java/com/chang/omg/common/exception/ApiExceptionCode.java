721c4e9fd3bef11f5e16feb364eeedfb90714469
changhyeon <changhyeon.h@kakao.com>
2024-03-20 23:07:50 +0900

    feat: 게임 타입을 뜻하는 Enum 객체 생성, DB에 저장 또는 조회시 작동하는 Converter 설정

--- src/main/java/com/chang/omg/common/exception/ApiExceptionCode.java
+++ src/main/java/com/chang/omg/common/exception/ApiExceptionCode.java
@@ -14,6 +14,7 @@ public enum ApiExceptionCode implements ExceptionCode {
     API_BAD_REQUEST(BAD_REQUEST, "API-002", "요청 정보가 잘못되었습니다."),
     API_FORBIDDEN(FORBIDDEN, "API-003", "요청 권한이 없습니다."),
     API_TOO_MANY_REQUESTS(TOO_MANY_REQUESTS, "API_004", "요청이 너무 많습니다."),
+    API_GAME_TYPE_IS_INVALID(BAD_REQUEST, "API_005", "게임 타입이 유효하지 않습니다."),
     ;
 
     private final HttpStatus status;
