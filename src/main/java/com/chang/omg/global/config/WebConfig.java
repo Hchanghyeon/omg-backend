662fbc651261672ad55f8dfadfcd7be93c279073
changhyeon <changhyeon.h@kakao.com>
2024-03-21 18:53:20 +0900

    feat: PathVariableConverter 및 조회 로직 구현

--- src/main/java/com/chang/omg/global/config/WebConfig.java
+++ src/main/java/com/chang/omg/global/config/WebConfig.java
@@ -1,10 +1,12 @@
-package com.chang.omg.infrastructure.config;
+package com.chang.omg.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.chang.omg.infrastructure.config.property.CorsProperties;
+import com.chang.omg.global.config.property.CorsProperties;
+import com.chang.omg.global.converter.GameTypeFromPathVariableConverter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(corsProperties.getUrls());
+    }
+
+    @Override
+    public void addFormatters(final FormatterRegistry registry) {
+        registry.addConverter(new GameTypeFromPathVariableConverter());
     }
 }
--- src/main/java/com/chang/omg/presentation/game/GameController.java
+++ src/main/java/com/chang/omg/presentation/game/GameController.java
@@ -1,12 +1,17 @@
 package com.chang.omg.presentation.game;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chang.omg.application.game.GameService;
+import com.chang.omg.domains.game.maplestorym.domain.GameCharacterSearchRank;
+import com.chang.omg.domains.game.maplestorym.domain.GameType;
 import com.chang.omg.presentation.game.dto.MapleStoryMCharacterInfoResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -29,5 +34,11 @@ public class GameController {
         );
 
         return ResponseEntity.ok(mapleStoryMCharacterInfoResponse);
+    }
+
+    @GetMapping("/{gameType}/rank")
+    public ResponseEntity<List<GameCharacterSearchRank>> getGameCharacterSearchRank(
+            @PathVariable final GameType gameType) {
+        return ResponseEntity.ok(gameService.getGameCharacterSearchRank(gameType));
     }
 }
